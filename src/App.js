0835aaf1b5b6dd730c8b9696b67d332bd55fabc7
AsthaTibrewal <75210550+AsthaTibrewal@users.noreply.github.com>
2020-12-09 18:55:59 +0530

    features updated

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import "./styles.css";
 var menuList = {
   breakfast: ["chapati", "alludam", "kulcha", "naan"],
   lunch: ["daal", "paneer", "rice", "fish", "chapati"],
-  dinner: ["daal", "paneer", "rice", "fish", "chapati"]
+  dinner: ["daal", "chicken", " fried-rice", "soya-beans", "chapati"]
 };
 export default function App() {
-  const [meaning, setMeaning] = useState("");
+  const [meaning, setMeaning] = useState([""]);
   var objectKeys = Object.keys(menuList);
-  console.log(objectKeys);
+
   function menuClickHandler(option) {
-    var meaning = menuList(option);
+    var meaning = menuList[option];
+
     setMeaning(meaning);
   }
+
   return (
     <div className="App">
       <h1>my menu list</h1>
-      {objectKeys.map(function (option) {
+      {objectKeys.map(function (option, index) {
         return (
-          <button onClick={() => menuClickHandler(option)}> {option}</button>
+          <button key={index} onClick={() => menuClickHandler(option)}>
+            {option}
+          </button>
         );
       })}
+      <ul>
+        {meaning.map((items) => {
+          return <li>{items}</li>;
+        })}
+      </ul>
     </div>
   );
 }
